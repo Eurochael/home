72cb5d1d8ba4027e72bf7cced1b41ad1cebdcee3
john <jaon14@gmail.com>
2017-02-28 14:52:09 +0900

    lazyload 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
+import VueLazyload from 'vue-lazyload'
+
+
+
+
 import router from './router'
 import {store} from './store'
+
+
+
+
 import App from './BackGroundTemplate/'
 
 Vue.config.productionTip = true
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: '/static/img/icon/Close.svg',
+  loading: '/static/img/icon/loading_spinner.gif',
+  attempt: 1,
+  // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+  listenEvents: [ 'scroll' ]
+})
+
+
+
+
+
 
 new Vue({
   el: '#app',
