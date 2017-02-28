5db1174e43710288eb3f5fad825224f1d4e48355
john <jaon14@gmail.com>
2017-02-28 20:49:45 +0900

    이미지 추가작업

--- src/page/main/index.vue
+++ src/page/main/index.vue
@@ -1,20 +1,48 @@
 <template>
-    <div class="testHeight">
+<div>
+    <div class="CarouselHeight">
         <Carousel></Carousel>
     </div>
+    <mPanel v-for="(mPDB, Key) in mPDBS" :key="Key" :GetData="mPDB">
+      <div slot="ContentDiv">
+        테스트
+        </div>
+      <div slot="ImgDiv">
+        테스트2
+        </div>        
+    </mPanel>
+</div>    
 </template>
 
 <script>
 import Carousel from './carousel'
+import mPanel from './mPanel'
 
 export default {
   name: 'MainPage',
   components:{
     Carousel,
+    mPanel
   },
   data () {
     return {
-      
+        mPDBS:[
+          {
+            test:'./static/img/hire/handshake.jpg',
+            isRv: false,
+
+          },
+          {
+            test:'./static/img/mainCarousel/3panel/greeting.svg',
+            isRv: true,
+
+          },
+          {
+            test:'./static/img/mainCarousel/3panel/handshake.png',
+            isRv: false,
+
+          },
+        ]      
     }
   }
 }
@@ -22,11 +50,12 @@ export default {
 
 
 <style scoped>
-.testHeight{
+.CarouselHeight{
   height : 100vh;
+  
 }
 @media(max-width:799px){
-  .testHeight{
+  .CarouselHeight{
       height : 500px;
     }  
 }
---
+++ src/page/main/mPanel.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="mPanel_background" :class="{reverse : GetData.isRv}">
+    <div class="mPanel_Content">
+        <slot name="ContentDiv"></slot>
+    </div>
+    <div class="mPanel_bkImg" v-lazy:background-image="GetData.test">
+        <slot name="ImgDiv"></slot>
+    </div>    
+</div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props:[
+      'GetData'
+  ],
+  data () {
+    return {
+      test:'./static/img/hire/handshake.jpg'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.mPanel_background{
+  height : 33vh;
+  display: flex;
+  
+}
+.mPanel_Content{
+    height : 33vh;
+    width : 40%;
+    display: flex;
+    justify-content: center;    
+    align-items: center;
+    
+}
+.mPanel_bkImg{
+    height : 33vh;
+    width : 60%;
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+.mPanel_background.reverse{
+    flex-direction: row-reverse;
+}
+          
+</style>
