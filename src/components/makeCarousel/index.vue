2d753bdbce139e7a02c144b52cd0bbbe6c5538ce
john <jaon14@gmail.com>
2017-02-28 17:53:19 +0900

    카로셀 테스트 파일

---
+++ src/components/makeCarousel/index.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="Carousel_Background">
+<slider width="100%"
+        height="100%"
+        animation="fade"
+        :interval="3000"
+        :speed="1000">
+  <slider-item :style="{ backgroundColor: '#3f51b5' }">테스트 가능?</slider-item>
+  <slider-item :style="{ backgroundColor: '#eee' }"></slider-item>
+  <slider-item :style="{ backgroundColor: '#f44336' }"></slider-item>
+  <slider-item :style="{ backgroundColor: '#3f51b5' }">테스트 가능?</slider-item>
+  <slider-item :style="{ backgroundColor: '#eee' }"></slider-item>  
+  <slider-item :style="{ backgroundColor: '#f44336' }"></slider-item>
+  <slider-item :style="{ backgroundColor: '#3f51b5' }">테스트 가능?</slider-item>  
+</slider>  
+
+</div>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+import { Slider, SliderItem } from 'vue-easy-slider'
+
+export default{
+    name : 'test',
+    components: {    
+        Carousel3d,
+        Slide,
+    Slider,
+    SliderItem        
+    },    
+    data(){
+        return{
+
+        }
+    },
+
+}
+</script>
+
+<style>
+.floatin{
+    display: block;
+    position: absolute;
+    top: 40%;
+    left: 0%;
+    color: white;
+}
+.Carousel_Background{
+    height : 100%;
+    width : 100%;
+    overflow: hidden;
+}
+
+
+
+          
+</style>
