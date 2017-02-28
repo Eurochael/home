0f111bc1b4f4aa72325cfcad51368b48eabee770
john <jaon14@gmail.com>
2017-02-28 18:43:46 +0900

    메인페이지추가

---
+++ src/page/main/carousel.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="Carousel_Background">
+<slider width="100%"
+        height="100%"
+        animation="fade"
+        :interval="3000"
+        :speed="1000">
+  <slider-item :style="{ backgroundColor: '#3f51b5' }">테스트 가능?</slider-item>
+  <slider-item :style="{ backgroundColor: '#eee' }"></slider-item>
+  <slider-item :style="{ backgroundColor: '#f44336' }"></slider-item>
+  <slider-item :style="{ backgroundColor: '#3f51b5' }">테스트 가능?</slider-item>
+  <slider-item :style="{ backgroundColor: '#eee' }"></slider-item>  
+  <slider-item :style="{ backgroundColor: '#f44336' }"></slider-item>
+  <slider-item :style="{ backgroundColor: '#3f51b5' }">테스트 가능?</slider-item>  
+</slider>  
+
+</div>
+</template>
+
+<script>
+import { Slider, SliderItem } from 'vue-easy-slider'
+
+export default{
+    name : 'test',
+    components: {    
+        Slider,
+        SliderItem        
+    },    
+    data(){
+        return{
+
+        }
+    },
+
+}
+</script>
+
+<style>
+.floatin{
+    display: block;
+    position: absolute;
+    top: 40%;
+    left: 0%;
+    color: white;
+}
+.Carousel_Background{
+    height : 100%;
+    width : 100%;
+    overflow: hidden;
+}
+
+
+
+          
+</style>
---
+++ src/page/main/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="testHeight">
+        <Carousel></Carousel>
+    </div>
+</template>
+
+<script>
+import Carousel from './carousel'
+
+export default {
+  name: 'MainPage',
+  components:{
+    Carousel,
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.testHeight{
+  min-height : 100vh;
+}
+</style>
