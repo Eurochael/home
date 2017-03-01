2a9b4517310c84a33be0ba35a3a4f15d8311a7df
john <jaon14@gmail.com>
2017-03-01 15:10:22 +0900

    history 수정중 value 이미지 width 값 문제로 임시저장

--- src/page/intro/history/index.vue
+++ src/page/intro/history/index.vue
@@ -1,5 +1,15 @@
 <template>
 <div class="introHeight">
+     <div class="FlexBox">
+        <div class="lineBox">
+            <div class="main">
+                우리의 핵심 가치
+            </div>
+            <div class="sub">
+                Our Core Values
+            </div>
+        </div>            
+    </div>    
     <div class="get">
         <div v-for="(DB, key) in DBS" :key="key" class="warpping">
             <div class="year">{{DB.YY}}</div>
@@ -47,8 +57,34 @@ export default {
 
 <style scoped>
 .introHeight{
-    height : 40vh;
+    height : 50vh;
 }
+
+.FlexBox{
+    display: flex;
+    justify-content: center;
+    align-items: center;    
+    flex-direction: column;
+
+    margin-bottom : 20px;
+
+}
+.lineBox{
+    border-left :10px solid black;
+    padding-left: 10px;
+}
+
+.main{
+    font-size: 4rem;
+    line-height: 4rem;
+    margin-bottom : 10px;
+}
+
+.sub{
+    font-size: 2rem;
+    line-height: 2rem;
+}
+
 .get{
     display: flex;
     flex-direction: column;
@@ -56,7 +92,7 @@ export default {
     justify-content: space-around;
     margin-left:50px;
 
-    height : 100%;
+    height : 70%;
     width : 150px;
 }
 .warpping{
